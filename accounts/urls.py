d17970e59700d6b53a8a077a2c90f5b52537e313
AYUSHJD098 <66873088+AYUSHJD098@users.noreply.github.com>
2020-08-10 17:03:37 +0530

    update and delte orders

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('customer/<str:pk>', views.customer, name="customer"),
     path('products/', views.products, name='product'),
-    path('addorder/', views.create_order, name='create_order'),
-    path('addcustomer/', views.create_customer, name='create_customer')
+    path('add_order/', views.create_order, name='create_order'),
+    path('update_order/<str:pk>', views.update_order, name='update_order'),
+    path('delete_order/<str:pk>', views.delete_order, name='delete_order'),
+    path('add_customer/', views.create_customer, name='create_customer')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import  *
 from .forms import *
@@ -30,15 +30,58 @@ def products(request):
 
 def create_order(request):
 
-	form = OrderForm
+	form = OrderForm()
 
+	if request.method == 'POST':	
+		form = OrderForm(request.POST)
+	
+		form.save()
+		return redirect("/")
 	context = {"form":form}
 	return render(request, 'accounts/order_form.html', context)
 
+def create_order(request):
+
+	form = OrderForm()
+
+	if request.method == 'POST':	
+		form = OrderForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect("/")
+	context = {"form":form}
+	return render(request, 'accounts/order_form.html', context)
+
+def update_order(request, pk):
+	
+	order = Order.objects.get(id=pk)
+	form = OrderForm(instance=order)
+	if request.method == 'POST':	
+		form = OrderForm(request.POST, instance=order)
+		if form.is_valid():
+			form.save()
+			return redirect("/")
+
+	context = {"form":form, 'order':order}
+	return render(request, 'accounts/order_form.html', context)
+
+
 def create_customer(request):
 
-	form = CustomerForm
-
+	form = CustomerForm()
+	if request.method == 'POST':
+		form = CustomerForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect("/")
 	context = {"form":form}
 	return render(request, 'accounts/customer_form.html', context)
 
+
+def delete_order(request, pk):
+	order = Order.objects.get(id=pk)
+	if request.method =='POST':
+		order.delete()
+		return redirect('/')
+	context = {'order':order}
+	return render(request,'accounts/delete.html', context)
